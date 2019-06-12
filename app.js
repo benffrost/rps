9b02657e4489d6857f1c0dc3936ac710143156d7
benffrost <benffrost@gmail.com>
2019-06-12 13:32:17 -0600

    init

---
+++ app.js
@@ -0,0 +1,73 @@
+let GameState = {
+  playOptions: ["Rock", "Paper", "Scissors"],
+  winOptions: ["beats", "loses to", "draws against"],
+  stateAI: "Rock",
+  stateHuman: "Rock",
+  stateWinner: "draws against",
+  winsAI: 0,
+  winsHuman: 0,
+
+  draw: function () {
+    document.querySelector("#score-human").innerHTML = "Score: " + this.winsHuman;
+    document.querySelector("#score-ai").innerHTML = "Score: " + this.winsAI;
+
+    document.querySelector("#humanstate").innerHTML = this.stateHuman;
+    document.querySelector("#aistate").innerHTML = this.stateAI;
+    document.querySelector("#winstate").innerHTML = this.stateWinner;
+  },
+
+  play: function (choice) {
+
+    this.stateHuman = choice;
+    this.stateAI = this.aiChoice(); //todo: add randomization here
+
+    if (this.stateHuman === this.stateAI) {
+      this.stateWinner = this.winOptions[2];
+    }
+    else if (this.stateHuman === "Rock") {
+      if (this.stateAI === "Scissors") {
+        this.stateWinner = this.winOptions[0];
+        this.winsHuman++;
+      }
+      else {
+        this.stateWinner = this.winOptions[1];
+        this.winsAI++;
+      }
+    }
+    else if (this.stateHuman === "Paper") {
+      if (this.stateAI === "Rock") {
+        this.stateWinner = this.winOptions[0];
+        this.winsHuman++;
+      }
+      else {
+        this.stateWinner = this.winOptions[1];
+        this.winsAI++;
+      }
+    }
+    else if (this.stateHuman === "Scissors") {
+      if (this.stateAI === "Paper") {
+        this.stateWinner = this.winOptions[0];
+        this.winsHuman++;
+      }
+      else {
+        this.stateWinner = this.winOptions[1];
+        this.winsAI++;
+      }
+    }
+
+
+    this.draw();
+  },
+
+  resetScore: function () {
+    this.winsAI = 0;
+    this.winsHuman = 0;
+    this.draw();
+  },
+
+  aiChoice: function () {
+    return this.playOptions[Math.floor(Math.random() * 3)];
+  }
+}
+
+GameState.draw();
